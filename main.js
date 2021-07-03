73b0ae580f729efb5a74917fbf9de8ee310f4a42
Amoghsaraswat <75422601+Amoghsaraswat@users.noreply.github.com>
2021-07-03 16:57:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
+nosex=0;
+nosey=0;
 function preload() {}
 function setup() {
     canvas=createCanvas(300,300);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on("pose",gotPoses);
 }
-function draw() {}
+function draw() {
+image(video,0,0,300,300);    
+}
 function snapshot() {
     save("Amogh.png");
+}
+function modelLoaded() {
+    console.log("pose net is loaded");
+}
+function gotPoses(results)
+{
+if(results.length>0) {
+ console.log(results);
+    nosex=results[0].pose.nose.x;
+    nosey=results[0].pose.nose.y;
+}
 }
